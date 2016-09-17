8aa3fc8400550249077d4d249558c258c518b2e6
bbathel12 <bricebathel@hotmail.com>
2016-09-16 22:49:55 -0500

    working version

--- get_tags.php
+++ get_tags.php
@@ -5,14 +5,14 @@ require_once 'Instagram-Grabber/vendor/autoload.php';
 header('content-type:json');
 header('Access-Control-Allow-Origin:*');
 $media = Bolandish\Instagram::getMediaByHashtag("bforbathel",350 );
-//$media2 = Bolandish\Instagram::getMediaByHashtag("nolawedding",350);
-foreach($media2 as $bforbathel){
-	array_push($media,$bforbathel);
+$media2 = Bolandish\Instagram::getMediaByHashtag("nolawedding",100);
+foreach($media as $bforbathel){
+	array_push($media2,$bforbathel);
 }
 /*echo "<pre>";
 echo var_dump(($media));
 echo "</pre>";*/
-foreach($media as $row){
+foreach($media2 as $row){
 	$images[]= array(
 		'src'=>$row->display_src,
 		//'caption'=>$row->caption,
